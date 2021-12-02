9fd78b907663aa7bbf255a9519428df8391870d3
Siva raman <shivasiva188@gmail.com>
2021-12-02 14:36:47 +0530

    ADDED: Some Code

--- ExercisesForProgrammers/ExercisesForProgrammers/Program.cs
+++ ExercisesForProgrammers/ExercisesForProgrammers/Program.cs
@@ -10,7 +10,25 @@ namespace ExercisesForProgrammers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\n -----------------------Retirement Calculator------------------------- \n");
+            Console.WriteLine("\n -----------------------Saying Hello------------------------- \n");
+            new Exercise01().ProgramSolution1();
+
+            Console.WriteLine("\n -------------Counting the Number of Characters-------------- \n");
+            new Exercise02().ProgramSolution2();
+
+            Console.WriteLine("\n ----------------------Printing Quotes----------------------- \n");
+            new Exercise03().ProgramSolution3();
+
+            Console.WriteLine("\n --------------------------Mad Lib--------------------------- \n");
+            new Exercise04().ProgramSolution4();
+
+            Console.WriteLine("\n ------------------------Sample Maths------------------------ \n");
+            new Exercise05().ProgramSolution5();
+
+            Console.WriteLine("\n --------------------Retirement Calculator------------------- \n");
+            new Exercise06().ProgramSolution6();
+
+            Console.WriteLine("\n -----------------Area of a Rectangular Room----------------- \n");
             new Exercise07().ProgramSolution7();
         }
     }
